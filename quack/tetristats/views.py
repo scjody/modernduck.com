7789d510b8139130efd7e5369ed2cf04003dc603
Jody McIntyre <scjody@modernduck.com>
2014-12-06 12:40:03 -0500

    Collect stats every 10s

--- quack/tetristats/views.py
+++ quack/tetristats/views.py
@@ -11,7 +11,19 @@ home = TemplateView.as_view(template_name='tetristats/home.html')
 
 @csrf_exempt
 def collector(request):
-    form = StatForm(request.POST)
+    uuid = request.POST.get('uuid')
+    old_stat = None
+    if uuid:
+        try:
+            old_stat = Stat.objects.get(uuid=uuid)
+        except Stat.DoesNotExist:
+            pass
+
+    if old_stat:
+        form = StatForm(request.POST, instance=old_stat)
+    else:
+        form = StatForm(request.POST)
+
     form.save()
 
     return HttpResponse(status=201)
