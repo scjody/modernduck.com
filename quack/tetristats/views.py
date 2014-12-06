050df9e1a864d8d4d5f8043f251987710f91daa1
Jody McIntyre <scjody@modernduck.com>
2014-12-06 14:02:54 -0500

    Add Plotly graph!

--- quack/tetristats/views.py
+++ quack/tetristats/views.py
@@ -1,5 +1,8 @@
 import datetime
 
+import plotly.plotly as py
+import plotly.graph_objs as pyg
+
 from django.db.models import Avg, Max, Sum
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -9,6 +12,7 @@ from django.views.generic.base import TemplateView
 from tetristats.models import Stat, StatForm
 
 
+py.sign_in("JodyMcintyre", "htzkacen0d")
 home = TemplateView.as_view(template_name='tetristats/home.html')
 
 
@@ -51,9 +55,43 @@ def show(request):
 
     agg['gas_rate__avg'] = agg['total_gas__sum'] / agg['time__sum']
 
+    update_plotly_graph()
+
     context = {
         'stats': stats,
         'agg': agg,
     }
 
     return render(request, 'tetristats/stats.html', context)
+
+
+def update_plotly_graph():
+    allstats = Stat.objects.all()
+    times = [s.time for s in allstats]
+    total_gas_consumptions = [s.total_gas for s in allstats]
+
+    trace1 = pyg.Scatter(
+        x=times,
+        y=total_gas_consumptions,
+        mode='markers',
+        marker=pyg.Marker(
+            color='rgb(255, 100, 0)',
+            size=12,
+            line=pyg.Line(
+                color='white',
+                width=0.5
+            ),
+        ),
+    )
+    data = pyg.Data([trace1])
+    layout = pyg.Layout(
+        title='Gas Consumption vs. Play Time',
+        xaxis=pyg.XAxis(
+            title='Play Time (seconds)',
+        ),
+        yaxis=pyg.YAxis(
+            title='Gas Consumption (cats)',
+        ),
+    )
+    figure = pyg.Figure(data=data, layout=layout)
+    py.plot(figure, filename='Gas Consumption vs. Play Time', auto_open=False)
