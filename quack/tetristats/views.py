79e397281c07e59d6b58d4b63421da62bef75ade
Jody McIntyre <scjody@modernduck.com>
2014-12-06 13:36:51 -0500

    Add aggregate statistics

--- quack/tetristats/views.py
+++ quack/tetristats/views.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.db.models import Avg, Max, Sum
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
@@ -36,4 +37,23 @@ def show(request):
     stats = Stat.objects.filter(timestamp__gte=show_start)
     if stats.count() < 5:
          stats = Stat.objects.all().order_by('-timestamp')[0:5]
-    return render(request, 'tetristats/stats.html', {'stats': stats})
+
+    agg = Stat.objects.all().aggregate(
+        Avg('total_gas'),
+        Avg('time'),
+        Sum('total_gas'),
+        Sum('time'),
+        Max('total_gas'),
+        Max('score'),
+        Max('lines'),
+        Max('time'),
+    )
+
+    agg['gas_rate__avg'] = agg['total_gas__sum'] / agg['time__sum']
+
+    context = {
+        'stats': stats,
+        'agg': agg,
+    }
+
+    return render(request, 'tetristats/stats.html', context)
