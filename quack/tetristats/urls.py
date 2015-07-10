67df4b73ef2046f592d52a3c43b4c93208b0a9b0
Jody McIntyre <scjody@modernduck.com>
2015-07-10 14:14:01 -0400

    Redirect tetristats base to firetetris.com

--- quack/tetristats/urls.py
+++ quack/tetristats/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import patterns, url
+from django.views.generic.base import RedirectView
 
 urlpatterns = patterns('',
-    url(r'^$', 'tetristats.views.home', name='home'),
+    #url(r'^$', 'tetristats.views.home', name='home'),
+    url(r'^$',
+        RedirectView.as_view(url='http://firetetris.com', permanent=False),
+        name='home'),
     url(r'^collector$', 'tetristats.views.collector', name='collector'),
     url(r'^stats$', 'tetristats.views.stats', name='stats'),
     url(r'^faq$', 'tetristats.views.faq', name='faq'),
